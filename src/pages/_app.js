8b73d25723fdf0760923745a272e58a26c4970be
P Joshi <joshipv2@gmail.com>
2026-05-31 15:51:09 -0400

    feat: Cloudinary image upload, Vercel config, build fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import '@/styles/globals.css';
 
+const CLERK_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
 export default function App({ Component, pageProps }) {
-  return (
-    <ClerkProvider {...pageProps}>
+  return CLERK_PUBLISHABLE_KEY ? (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} {...pageProps}>
       <Component {...pageProps} />
     </ClerkProvider>
+  ) : (
+    <Component {...pageProps} />
   );
 }
